1458eeb707483beb8a3673de067febcf2577ff81
RomanKuzhekov <profet@inbox.ru>
2022-10-18 09:12:27 +0300

    Подчистил код

--- views/admin/index.php
+++ views/admin/index.php
@@ -6,7 +6,6 @@
     <p><b>Структура данных:</b></p>
     <b><a href="/admin/add/" class="add">&#10010;</a></b>
     <? if (!empty($data)) { ?>
-
         <ul class='list-items'>
             <? foreach ($data as $id => $item) {?>
                 <li><?=$item?> <span class="tools"><a href="/admin/add/<?=$id?>" class="add">&#10010;</a> <a href="/admin/edit/<?=$id?>" class="edit">&#9998;</a> <a href="/admin/delete/<?=$id?>" id="delete" class="delete">&#10006;</a></span></li>
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -13,18 +13,6 @@
 
     <div class="content">
         <h1>Тестовое задание</h1>
-<!--        <ul>-->
-<!--            <li><a href="/" class="active">Главная</a></li>-->
-<!--            --><?php //if(!empty($_SESSION['sid'])): ?>
-<!--                <li>Личный кабинет:</li>-->
-<!--                <li><a href="/auth/profile" style="text-decoration: underline; color: blue;">--><?//=ucfirst($_SESSION['name'])?><!-- (--><?//=$_SESSION['login']?><!--)</a></li>-->
-<!--                <li><a href='/auth/logout'><u>Выйти</u> </a></li>-->
-<!--            --><?php //else: ?>
-<!--                <li><a href='/auth/login'><u>Войти</u></a></li>-->
-<!--                <li><a href='/auth/signup'><u>Регистрация</u></a></li>-->
-<!--            --><?php //endif; ?>
-<!--        </ul>-->
-<!--        <hr>-->
 
         <?=$content?>
     </div>
